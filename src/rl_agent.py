9530ad1b5614ee766f08a787ca8f2bd01db2c397
vandanamonappa07 <97655515+vandanamonappa07@users.noreply.github.com>
2026-04-18 16:36:09 +0530

    Update rl_agent.py

--- src/rl_agent.py
+++ src/rl_agent.py
@@ -1,6 +1,6 @@
 """
 File: rl_agent.py
-Author: Vandan V.S
+Author: Vandana B.S
 Description:
 Implements a Q-learning based reinforcement learning agent for
 adaptive preprocessing selection.
@@ -20,33 +20,31 @@ class PreprocessingRLAgent:
 
 
     def get_state_key(self, features):
-        """
-        Convert feature vector to discrete state representation.
-        """
-        return tuple((features * 10).astype(int))
+        return tuple((np.array(features) * 10).astype(int))
 
 
     def select_action(self, state_features):
-        """
-        Select action using epsilon-greedy strategy.
-        """
-
-        key = self.get_state_key(state_features)
-
-        if np.random.rand() < self.epsilon or key not in self.q_table:
-            return np.random.randint(self.n_actions)
-
-        return int(np.argmax(self.q_table[key]))
-
-
-    def update_q(self, state_features, action, reward):
-        """
-        Update Q-table values.
-        """
 
         key = self.get_state_key(state_features)
 
         if key not in self.q_table:
             self.q_table[key] = np.zeros(self.n_actions)
 
-        self.q_table[key][action] += self.lr * (reward - self.q_table[key][action])
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(self.n_actions)
+
+        return int(np.argmax(self.q_table[key]))
+
+
+    def update_q(self, state_features, action, reward):
+
+        key = self.get_state_key(state_features)
+
+        if key not in self.q_table:
+            self.q_table[key] = np.zeros(self.n_actions)
+
+        gamma = 0.9
+
+        self.q_table[key][action] += self.lr * (
+            reward + gamma * np.max(self.q_table[key]) - self.q_table[key][action]
+        )
